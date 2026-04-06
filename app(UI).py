3c90df7b9c61081c61413e2328146033d8868b12
Shrawani <shrawaniasarkar@gmail.com>
2026-04-06 14:50:30 +0530

    Added Streamlit UI

---
+++ app(UI).py
@@ -0,0 +1,18 @@
+import streamlit as st
+import requests
+
+st.title("📩 Spam Classifier")
+st.info("First request may take ~1 min (free hosting)")
+
+text = st.text_area("Enter email text")
+
+if st.button("Predict"):
+    url = "https://spam-classifier-api-xv9i.onrender.com"
+    
+    response = requests.post(url, json={"text": text})
+    
+    if response.status_code == 200:
+        result = response.json()
+        st.success(f"Prediction: {result['prediction']}")
+    else:
+        st.error("Error in prediction")
